5d401fc806d6189e660d3a6e62d98ac333dfeb98
Crunk <96680644+Truongphi20@users.noreply.github.com>
2025-06-23 12:29:55 -0400

    update: testing on testcase 1

--- tests/TestFile.hpp
+++ tests/TestFile.hpp
@@ -8,7 +8,8 @@ class TestFile
 {
     public:
     std::filesystem::path get_tmp_file(std::string file_name);
-    std::filesystem::path create_tmp_file(std::string file_context, std::string file_name);
+    std::filesystem::path create_tmp_file(std::string file_context, std::string prefix);
+    std::filesystem::path get_tmp_vcf_file(std::string file_key);
 
     private:
     static std::map<std::string, std::string> file_menu;
