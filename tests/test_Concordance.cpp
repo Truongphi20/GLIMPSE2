312787ac9c24eef37ee9228d44cb443cc19ad257
Crunk <96680644+Truongphi20@users.noreply.github.com>
2025-06-24 01:58:37 -0400

    update: success dry-run test vcf itself

--- tests/test_Concordance.cpp
+++ tests/test_Concordance.cpp
@@ -12,14 +12,16 @@ TEST(Initial, test_create_tmp_vcf_file) {
                                   "##contig=<ID=1,length=1000000>\n"
                                   "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">\n"
                                   "##FORMAT=<ID=DS,Number=1,Type=Float,Description=\"Genotype Dosage\">\n"
+                                  "##INFO=<ID=MAF,Number=1,Type=Float,Description=\"Minor Allele Frequency\">\n"
                                   "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tsample1\tsample2\tsample3\n"
-                                  "1\t123456\t.\tA\tG\t100\tPASS\t.\tGT:DS\t0|0:0.0\t0|1:1.0\t1|1:2.0\n"
-                                  "1\t123789\t.\tC\tT\t99\tPASS\t.\tGT:DS\t0|1:0.8\t0|0:0.1\t1|1:1.9\n"
-                                  "1\t200123\trs123\tG\tA\t300\tPASS\t.\tGT:DS\t1|1:0.2\t0|1:0.7\t1|0:1.1\n"
-                                  "1\t250000\t.\tT\tC\t150\tPASS\t.\tGT:DS\t1|1:1.95\t0|0:0.05\t0|1:1.2\n";
+                                  "1\t123456\t.\tA\tG\t100\tPASS\tMAF=0.1\tGT:DS\t0|0:0.0\t0|1:1.0\t1|1:2.0\n"
+                                  "1\t123789\t.\tC\tT\t99\tPASS\tMAF=0.2\tGT:DS\t0|1:0.8\t0|0:0.1\t1|1:1.9\n"
+                                  "1\t200123\trs123\tG\tA\t300\tPASS\tMAF=0.3\tGT:DS\t1|1:0.2\t0|1:0.7\t1|0:1.1\n"
+                                  "1\t250000\t.\tT\tC\t150\tPASS\tMAF=0.4\tGT:DS\t1|1:1.95\t0|0:0.05\t0|1:1.2\n"
+                                  "1\t260000\t.\tA\tC\t150\tPASS\tMAF=0.5\tGT:DS\t0|1:1.55\t1|0:0.15\t1|1:1.5\n";
 
   // Get context of tmp file
-  fs::path vcf_path = TestFile().get_tmp_file("simple_file");
+  fs::path vcf_path = TestFile().get_tmp_file("simple_pos_file");
   std::cout << "Temp VCF file created at: " << vcf_path << '\n';
   std::ifstream actual_file_flow(vcf_path);
   std::string actual_context((std::istreambuf_iterator<char>(actual_file_flow)), std::istreambuf_iterator<char>());
